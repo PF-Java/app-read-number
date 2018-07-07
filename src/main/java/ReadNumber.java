af6e3f06a5be005300afa2e272887974a9d01416
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-07-08 00:47:54 +0700

    write read with number 0 -999

--- src/main/java/ReadNumber.java
+++ src/main/java/ReadNumber.java
@@ -3,149 +3,140 @@ import java.util.Scanner;
 public class ReadNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Your number: ");
+        System.out.print("Your number: ");
         int number = scanner.nextInt();
+        System.out.print("\nNumber " + number + " read is ");
         String readNumber = "";
 
-        System.out.print("Number " + number + " read is ");
-        switch (number) {
-            case 0:
-                readNumber = "Zero";
-                break;
+        int hangTram = number / 100;
+        switch (hangTram) {
             case 1:
-                readNumber = "One";
+                readNumber = "One Hundred ";
                 break;
             case 2:
-                readNumber = "Two";
+                readNumber = "Two Hundred ";
                 break;
             case 3:
-                readNumber = "Three";
+                readNumber = "Three Hundred ";
                 break;
             case 4:
-                readNumber = "Four";
+                readNumber = "Four Hundred ";
                 break;
             case 5:
-                readNumber = "Five";
+                readNumber = "Five Hundred ";
                 break;
             case 6:
-                readNumber = "Six";
+                readNumber = "Six Hundred ";
                 break;
             case 7:
-                readNumber = "Seven";
+                readNumber = "Seven Hundred ";
                 break;
             case 8:
-                readNumber = "Eight";
+                readNumber = "Eight Hundred ";
                 break;
             case 9:
-                readNumber = "Night";
+                readNumber = "Night Hundred ";
                 break;
         }
 
-        if (10 <= number && number < 20) {
-
-            switch (number) {
-                case 10:
-                    readNumber = "Ten";
-                    break;
-                case 11:
-                    readNumber = "Eleven";
-                    break;
-                case 12:
-                    readNumber = "Twelve";
-                    break;
-                case 13:
-                    readNumber = "Thirteen";
-                    break;
-                case 15:
-                    readNumber = "Fifteen";
-                    break;
-                case 18:
-                    readNumber = "Eighteen";
-                    break;
-                default:{
-                    number = number % 10;
-                    switch (number) {
-                        case 4:
-                            readNumber = "Four";
-                            break;
-                        case 6:
-                            readNumber = "Six";
-                            break;
-                        case 7:
-                            readNumber = "Seven";
-                            break;
-                        case 9:
-                            readNumber = "Nine";
-                            break;
-                    }
-                    readNumber = readNumber + "teen.";
-                }
-            }
-        }
-
-        if (20 <= number && number < 100) {
-            switch (number / 10) {
+        number = number % 100;
+        int hangChuc = number / 10;
+            switch (hangChuc) {
                 case 2:
-                    readNumber = "Twenty ";
+                    readNumber += "Twenty ";
                     break;
                 case 3:
-                    readNumber = "Thirty ";
+                    readNumber += "Thirty ";
                     break;
                 case 4:
-                    readNumber = "Forty ";
+                    readNumber += "Forty ";
                     break;
                 case 5:
-                    readNumber = "Fifty ";
+                    readNumber += "Fifty ";
                     break;
                 case 6:
-                    readNumber = "Sixty ";
+                    readNumber += "Sixty ";
                     break;
                 case 7:
-                    readNumber = "Seventy ";
+                    readNumber += "Seventy ";
                     break;
                 case 8:
-                    readNumber = "Eighty ";
+                    readNumber += "Eighty ";
                     break;
                 case 9:
-                    readNumber = "Ninety ";
+                    readNumber += "Ninety ";
                     break;
             }
 
-            number = number % 10;
-            switch (number) {
-//                case 0:
-//                    readNumber = readNumber;
-//                    break;
+        number = number % 10;
+        int hangDonVi = number;
+        if (hangChuc == 1) {
+            switch (hangDonVi) {
+                case 0:
+                    readNumber += "Ten ";
+                    break;
                 case 1:
-                    readNumber = readNumber + "One";
+                    readNumber += "Eleven ";
                     break;
                 case 2:
-                    readNumber = readNumber + "Two";
+                    readNumber += "Twelve ";
                     break;
                 case 3:
-                    readNumber = readNumber + "Three";
-                    break;
-                case 4:
-                    readNumber = readNumber + "Four";
+                    readNumber += "Thirteen ";
                     break;
                 case 5:
-                    readNumber = readNumber + "Five";
-                    break;
-                case 6:
-                    readNumber = readNumber + "Six";
-                    break;
-                case 7:
-                    readNumber = readNumber + "Seven";
+                    readNumber += "Fifteen ";
                     break;
                 case 8:
-                    readNumber = readNumber +"Eight";
+                    readNumber += "Eighteen ";
+                    break;
+                case 4:
+                    readNumber += "Fourteen ";
+                    break;
+                case 6:
+                    readNumber += "Sixteen ";
+                    break;
+                case 7:
+                    readNumber += "Seventeen ";
                     break;
                 case 9:
-                    readNumber = readNumber + "Night";
+                    readNumber += "Nineteen ";
                     break;
             }
-            ;
+        } else {
+            switch (hangDonVi) {
+            case 1:
+                readNumber += "One";
+                break;
+            case 2:
+                readNumber += "Two";
+                break;
+            case 3:
+                readNumber += "Three";
+                break;
+            case 4:
+                readNumber += "Four";
+                break;
+            case 5:
+                readNumber += "Five";
+                break;
+            case 6:
+                readNumber += "Six";
+                break;
+            case 7:
+                readNumber += "Seven";
+                break;
+            case 8:
+                readNumber += "Eight";
+                break;
+            case 9:
+                readNumber += "Night";
+                break;
+            }
         }
-        System.out.println(readNumber);
+        if (number == 0)
+            readNumber = "Zero";
+        System.out.print(readNumber);
     }
 }
+
